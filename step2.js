7c8c6047ed1e767e29547f9d54d853ab595b51a4
Ekaterina Chikhacheva <ekaterinachikhacheva@Ekaterinas-MacBook-Air.local>
2021-09-06 23:54:44 -0400

    Lame unfinished app

---
+++ step2.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const process = require('process')
+
+/** read file at path and print it out. */
+
+function cat(path){
+    fs.readFile(path, 'utf8', function(err,data) {
+        if (err) {
+            console.log(`Error reading ${path} : ${err}`)
+            process.exit(1)
+        } else {
+        console.log("We're good")
+        console.log(`file contents are: ${data}`)
+        }
+    })
+}
+// process.argv is an array of command-line arguments given to start this program
+// cat(process.argv[2])
+
+async function webCat(url){
+    res = await axios.get(url)
+    console.log(res)
+}
+
+webCat(process.argv[2])
